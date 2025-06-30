c4d6cb45f74704c80172d9d87381c7699101afaf
Zaid Khan <zaidbinasif468@gmail.com>
2025-06-30 12:07:10 +0500

    load products from the backend end s

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,37 +1,42 @@
-import {cart, addToCart} from '../data/cart.js';
-import {products} from '../data/products.js';
-import {formatCurrency} from './utils/money.js';
+import { cart, addToCart } from '../data/cart.js';
+import { products, loadProducts } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
-let productsHTML = '';
+loadProducts(renderProductsGrid);
 
-products.forEach((product) => {
-  productsHTML += `
+
+function renderProductsGrid() {
+
+  let productsHTML = '';
+
+  products.forEach((product) => {
+    productsHTML += `
     <div class="product-container">
       <div class="product-image-container">
-        <img class="product-image"
-          src="${product.image}">
+        <img alt="Not Found" class="product-image"
+        src="${product.image}">
       </div>
-
+      
       <div class="product-name limit-text-to-2-lines">
         ${product.name}
       </div>
-
+      
       <div class="product-rating-container">
-        <img class="product-rating-stars"
+        <img alt="Not Found" class="product-rating-stars"
           src="${product.getStarsUrl()}">
         <div class="product-rating-count link-primary">
-          ${product.rating.count}
+        ${product.rating.count}
         </div>
       </div>
 
       <div class="product-price">
-        ${product.getPrice()}
+      ${product.getPrice()}
       </div>
-
+      
       <div class="product-quantity-container">
         <select>
-          <option selected value="1">1</option>
-          <option value="2">2</option>
+        <option selected value="1">1</option>
+        <option value="2">2</option>
           <option value="3">3</option>
           <option value="4">4</option>
           <option value="5">5</option>
@@ -40,16 +45,16 @@ products.forEach((product) => {
           <option value="8">8</option>
           <option value="9">9</option>
           <option value="10">10</option>
-        </select>
-      </div>
+          </select>
+          </div>
 
       ${product.extraInfoHTML()}
 
       <div class="product-spacer"></div>
 
       <div class="added-to-cart">
-        <img src="images/icons/checkmark.png">
-        Added
+      <img alt="Not Found" src="images/icons/checkmark.png">
+      Added
       </div>
 
       <button class="add-to-cart-button button-primary js-add-to-cart"
@@ -58,26 +63,27 @@ products.forEach((product) => {
       </button>
     </div>
   `;
-});
-
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
-
-function updateCartQuantity() {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
   });
 
-  document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantity;
-}
+  document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) => {
-    button.addEventListener('click', () => {
-      const productId = button.dataset.productId;
-      addToCart(productId);
-      updateCartQuantity();
+  function updateCartQuantity() {
+    let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
     });
-  });
+
+    document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
+  }
+
+  document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+      button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+        addToCart(productId);
+        updateCartQuantity();
+      });
+    });
+};
